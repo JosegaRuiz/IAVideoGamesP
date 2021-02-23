b52c5314382e4f21e6da8375673e95f1077f4c44
JosegaRuiz <72882621+JosegaRuiz@users.noreply.github.com>
2021-02-23 21:48:06 +0100

    Versión 1.0
    Añadido el proyecto después de la primera semana, tratados los temas: 
    -Representación gráfica de equipos y su HUD (definido en canvas)
    -Movimiento de Leaders y Walkers
    -Sistema de Choques elásticos

---
+++ Assets/Scripts/scriptLeader1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scriptLeader1: MonoBehaviour
+{
+
+	public float speed;
+	public Text countText;
+
+	// Leader1's health
+	public int maxHealth = 100;
+	public int currentHealth;
+	public HealthBar healthBar;
+
+	//Coin's counter
+	private int count;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentHealth=maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+		speed = 7f;
+		count = 0;
+		SetCountText();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//Acción para descrementar la vida del jugador
+		//AQUI
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			TakeDamage(20);
+		}
+		//Acción para descrementar la vida del jugador
+		transform.Translate(speed * Input.GetAxis("HorizontalWASD") * Time.deltaTime, 0f, speed * Input.GetAxis("VerticalWASD") * Time.deltaTime);
+	}
+	
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		healthBar.SetHealth(currentHealth);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString() + " Points";
+        //if (count >= 10)
+        //{
+        //    winText.text = "WIN";
+        //}
+    }
+}
---
+++ Assets/Scripts/scriptWalkers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class scriptWalkers : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    [Header("AISettings")]
+    public Vector3 Positions = new Vector3(0, 0, 0);
+    public float RandomXposition = 0;
+    public float RandomZposition = 0;
+    public NavMeshAgent Walker;
+
+
+    void Start()
+    {
+
+        SetTarget();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Positions = new Vector3(RandomXposition, transform.position.y, RandomZposition);
+
+        Walker.SetDestination(Positions);
+
+        if (transform.position == Positions)
+        {
+            SetTarget();
+        }
+    }
+
+    void SetTarget()
+    {
+        RandomXposition = Random.Range(20, -20);
+        RandomZposition = Random.Range(20, -20);
+    }
+}
